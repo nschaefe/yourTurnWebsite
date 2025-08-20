91a9b5a60f2d922faed7f766e88ad09c94d02648
Nicolas Schaefer <nicolas.schaefer@sap.com>
2025-08-20 13:19:08 +0200

    main view fixes, refactoring

--- _app/immutable/entry/start.B4C_CNnR.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BH3BguD2.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.rChIZ3z_.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CyvwQZG6.js";export{o as load_css,r as start};
